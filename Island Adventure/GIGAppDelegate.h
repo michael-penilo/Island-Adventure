1a18edd22e463565ae77847d7e5e33e7a6025708
Michael Penilo <michaelpenilo@Michaels-MacBook-Air.local>
2014-09-09 13:32:15 +1000

    Initial Commit

---
+++ Island Adventure/GIGAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  GIGAppDelegate.h
+//  Island Adventure
+//
+//  Created by Michael Penilo on 9/09/2014.
+//  Copyright (c) 2014 Get IT Going. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GIGAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Island Adventure/GIGViewController.h
@@ -0,0 +1,13 @@
+//
+//  GIGViewController.h
+//  Island Adventure
+//
+//  Created by Michael Penilo on 9/09/2014.
+//  Copyright (c) 2014 Get IT Going. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GIGViewController : UIViewController
+
+@end
